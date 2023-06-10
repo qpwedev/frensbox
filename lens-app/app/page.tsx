89cc92acb748aa08f7f5d05a99450aab9ee94056
qpwedev <russfdk@gmail.com>
2023-06-10 21:25:08 +0200

    add profile search

--- lens-app/app/page.tsx
+++ lens-app/app/page.tsx
@@ -9,11 +9,23 @@ import {
 } from "@lens-protocol/react-web";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
+import { useState } from "react";
 
 export default function Home() {
   const { data: profiles } = useExploreProfiles({
     limit: 25,
   });
+
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearchChange = (event: any) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleSearchSubmit = (event: any) => {
+    event.preventDefault(); // this prevents the page from reloading
+    // the navigation will be handled by the Link component
+  };
 
   const { execute: login, isPending: isLoginPending } = useWalletLogin();
   const { execute: logout } = useWalletLogout();
@@ -60,6 +72,27 @@ export default function Home() {
           </button>
         </div>
       )}
+
+      <form onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          value={searchInput}
+          onChange={handleSearchChange}
+          placeholder="Search profiles"
+          className="mt-2 px-4 py-2 bg-white text-black rounded"
+        />
+        {searchInput && (
+          <Link href={`/profile/${searchInput}.lens`}>
+            <button
+              type="submit"
+              className="ml-2 px-4 py-2 bg-white text-black rounded"
+            >
+              Go
+            </button>
+          </Link>
+        )}
+      </form>
+
       <h1 className="text-5xl">My Lens App</h1>
       {profiles?.map((profile, index) => (
         <Link href={`/profile/${profile.handle}`} key={index}>
@@ -82,62 +115,3 @@ export default function Home() {
     </div>
   );
 }
-
-// "use client";
-// import {
-//   useWalletLogin,
-//   useWalletLogout,
-//   useActiveProfile,
-// } from "@lens-protocol/react-web";
-// import { useAccount, useConnect, useDisconnect } from "wagmi";
-// import { InjectedConnector } from "wagmi/connectors/injected";
-
-// export default function Authentication() {
-//   const { execute: login, isPending: isLoginPending } = useWalletLogin();
-//   const { execute: logout } = useWalletLogout();
-//   const { data: wallet, loading } = useActiveProfile();
-//   const { isConnected } = useAccount();
-//   const { disconnectAsync } = useDisconnect();
-
-//   const { connectAsync } = useConnect({
-//     connector: new InjectedConnector(),
-//   });
-
-//   const onLoginClick = async () => {
-//     if (isConnected) {
-//       await disconnectAsync();
-//     }
-//     const { connector } = await connectAsync();
-//     if (connector instanceof InjectedConnector) {
-//       const signer = await connector.getSigner();
-//       await login(signer);
-//     }
-//   };
-
-//   return (
-//     <div className="flex flex-col p-12 items-start">
-//       {loading && <p>Loading...</p>}
-//       {!wallet && !loading && (
-//         <button
-//           className="mt-2 px-6 py-1 bg-white text-black rounded"
-//           disabled={isLoginPending}
-//           onClick={onLoginClick}
-//         >
-//           Sign in
-//         </button>
-//       )}
-//       {wallet && !loading && (
-//         <div>
-//           <h3 className="text-3xl">{wallet.handle}</h3>
-//           <p>{wallet.bio}</p>
-//           <button
-//             onClick={logout}
-//             className="mt-2 px-6 py-1 bg-white text-black rounded"
-//           >
-//             Sign out
-//           </button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
